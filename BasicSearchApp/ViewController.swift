a44bd81abdfee5a9867e5581a3a13998ad059a19
Emir <alkalemir@gmail.com>
2022-05-31 19:31:35 +0300

    Initial Commit

---
+++ BasicSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicSearchApp
+//
+//  Created by Emir Alkal on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
